8ee49433c668ed7656ad85084458a588d1753649
srajankumar <kumarsrajan02@gmail.com>
2023-05-24 20:50:16 +0530

    4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+let name = "Srajan";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <div className="container">
+          <h1>Hello {name}</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+            illo doloremque debitis laboriosam corrupti quod assumenda,
+            doloribus dolor optio harum, at aspernatur.
+          </p>
+        </div>
+      </nav>
+    </>
   );
 }
 
